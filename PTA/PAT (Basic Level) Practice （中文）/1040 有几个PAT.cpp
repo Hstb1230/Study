8d7bffac40dbca436a8d3b14ffd317b750081349
Hstb <i@hstb1230.com>
2020-02-12 02:37:08 +0800

    Update 1040 有几个PAT.cpp
    Messy optimization

--- PTA/PAT (Basic Level) Practice （中文）/1040 有几个PAT.cpp
+++ PTA/PAT (Basic Level) Practice （中文）/1040 有几个PAT.cpp
@@ -4,18 +4,23 @@ using namespace std;
 int main() {
 	string pat;
 	getline(cin, pat);
-	int count = 0, tmp = 0, a = 0, t = 0;
-	for(int i = pat.length() - 1; i>= 0; i--) {
-		if(pat[i] == 'T')
-			t++;
-		else if(pat[i] == 'A') {
-			a++;
-			tmp += t;
-		} else if(pat[i] == 'P') {
-			count += tmp;
-			count %= 1000000007;
+	int count = 0, tmp = 0, t = 0;
+	// 从后往前数T的数量, 遇到A时计算一次一个P能组合的数量, 等遇到P时再增加组合数
+	// 如果没遇到P, 在遇到A时直接添加, 就会出现误统计情况(因为AT前面不一定有P)
+	for(int i = pat.length(); i-- != 0; ) {
+		switch(pat[i]) {
+			case 'T':
+				++t;
+				break;
+			case 'A':
+				tmp += t;
+				break;
+			case 'P':
+				count += tmp;
+				count %= 1000000007;
+				break;
 		}
-//		printf("%d %d | %d %d\n", t, a, tmp, count);
 	}
 	cout << count;
+	return 0;
 }
