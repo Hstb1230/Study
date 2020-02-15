5f953d1499505c6bbfea58acc118378eab74c956
Hstb <i@hstb1230.com>
2020-02-15 23:13:05 +0800

    Update 1045 快速排序.cpp
    the answer is right, and it is a new way.

--- PTA/PAT (Basic Level) Practice （中文）/1045 快速排序.cpp
+++ PTA/PAT (Basic Level) Practice （中文）/1045 快速排序.cpp
@@ -1,38 +1,51 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
-int getMin(int * begin, int * end) {
-	int * mini = begin;
-	for(int * i = begin + 1; i < end; i++) {
-		if(*i < *mini)
-			mini = i;
+void printArray(int b[], int t) {
+	printf("b: ");
+	for(int i = 0; i < t; i++) {
+		printf("%d ", b[i]);
 	}
-	return *mini;
+	printf("\n");
 }
+
 int main() {
 	int n;
-	cin >> n;
+	scanf("%d", &n);
 	int a[n];
 	for(int i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
 	// maxv is the maximun of the left part
-	// minv is the minimun of the right part
 	int maxv = a[0];
-	int minv = a[n - 1];
+	// b is the result
 	int b[n];
+	// t is the size of b
 	int t = 0;
 	for(int i = 0; i < n; i++) {
+		// 判断 a当前元素 是否比 b的最后一个元素 大
+		// 如果是, 就说明b中有不是主元的元素,
+		// 需要回溯, 将这些元素踢除
+		// if a[i] > b[end], then the b has some wrong keys.
+		if(t > 0 && b[t - 1] > a[i]) {
+			for(int j = t - 1; j >= 0; j--) {
+				if(b[j] > a[i]) t--;
+				else break;
+			}
+//			printArray(b, t);
+		}
+		// the key must be maximun of left.
 		if(a[i] < maxv) continue;
 		maxv = a[i];
-		if(a[i] > getMin(a + i, a + n)) continue;
+		// the key must min than right
+		if(i + 1 < n && a[i] > a[i + 1]) continue;
 		b[t++] = a[i];
+//		printArray(b, t);
 	}
 	printf("%d\n", t);
-	sort(b, b + t);
 	for(int i = 0; i < t; i++) {
-		if(i) printf(" ");
+		if(i > 0) printf(" ");
 		printf("%d", b[i]);
 	}
+	printf("\n");
 }
