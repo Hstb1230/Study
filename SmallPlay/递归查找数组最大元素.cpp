bd7fb80a13fd0e76d871ebba614157abe8f10825
Hstb1230 <i@hstb1230.com>
2020-06-01 18:14:22 +0800

    Create 递归查找数组最大元素.cpp

---
+++ SmallPlay/递归查找数组最大元素.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int max(vector<int> v)
+{
+	switch(v.size())
+	{
+		case 0:
+			return 0;
+		case 1:
+			return v[0];
+		case 2:
+			return v[0] < v[1] ? v[1] : v[0];
+	}
+	vector<int> b(v.begin() + 1, v.end());
+	int m = max(b);
+	return v[0] < m ? m : v[0];
+}
+
+int main()
+{
+	vector<int> v(100);
+	for(int i = 0; i < 100; i++)
+		v[i] = i;
+	printf("%d", max(v));
+}
