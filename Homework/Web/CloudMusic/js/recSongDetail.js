53f925b98aaf531e205643efac1566333505b0e9
Hstb1230 <i@hstb1230.com>
2020-06-12 23:29:54 +0800

    Fix the detail showing

--- Homework/Web/CloudMusic/js/recSongDetail.js
+++ Homework/Web/CloudMusic/js/recSongDetail.js
@@ -38,36 +38,56 @@ function $make(e) {
 
 axios.all( [ getPlaylistDetail(), getPlaylistComment() ] )
 .then( axios.spread(
-    (deatil, comment) => {
-        console.log(deatil);
+    (detail, comment) => {
+        console.log(detail);
         console.log(comment);
 
         let coverImg = document.querySelector('.cover-img > img');
         let bgImg = document.querySelector('.bg-blur > img');
         let playCount = document.querySelector('.cover-img > .play-number');
         // 替换封面
-        bgImg.src = coverImg.src = deatil.data.playlist.coverImgUrl;
+        bgImg.src = coverImg.src = detail.data.playlist.coverImgUrl;
         // 替换播放数据
-        if(deatil.data.playlist.playCount > 1e8)
-            playCount.innerHTML = `${Math.floor(deatil.data.playlist.playCount / 1e8 * 10) / 10}亿`;
-        else if(deatil.data.playlist.playCount > 1e4)
-            playCount.innerHTML = `${Math.floor(deatil.data.playlist.playCount / 1e4 * 10) / 10}万`;
+        if(detail.data.playlist.playCount > 1e8)
+            playCount.innerHTML = `${Math.floor(detail.data.playlist.playCount / 1e8 * 10) / 10}亿`;
+        else if(detail.data.playlist.playCount > 1e4)
+            playCount.innerHTML = `${Math.floor(detail.data.playlist.playCount / 1e4 * 10) / 10}万`;
         else
-            playCount.innerHTML = deatil.data.playlist.playCount;
+            playCount.innerHTML = detail.data.playlist.playCount;
         
         let title = document.querySelector('.title > h3');
         let authorName = document.querySelector('.author > p');
         let authorAvatar = document.querySelector('.author > .avatar > img');
         // 歌单标题
-        document.title = deatil.data.playlist.name;
-        title.innerText = deatil.data.playlist.name;
+        document.title = detail.data.playlist.name;
+        title.innerText = detail.data.playlist.name;
         // 歌单作者的昵称和头像
-        authorName.innerText = deatil.data.playlist.creator.nickname;
-        authorAvatar.src = deatil.data.playlist.creator.avatarUrl;
-
+        authorName.innerText = detail.data.playlist.creator.nickname;
+        authorAvatar.src = detail.data.playlist.creator.avatarUrl;
+        // 作者类型
+        // console.log(detail.data.playlist.creator);
+        if(detail.data.playlist.creator.accountStatus === 0)
+        {
+            // 账户状态正常
+            let createType = $('.author > .avatar > span');
+            switch (detail.data.playlist.creator.userType) {
+                // 音乐达人
+                case 200:
+                    createType.setAttribute('class', 'star');
+                    break;
+                // 音乐人
+                case 4:
+                    createType.setAttribute('class', 'musician');
+                    break;
+            
+                default:
+                    createType.removeAttribute('class');
+                    break;
+            }
+        }
         // 设置标签
         let tag = $('.intro_title');
-        deatil.data.playlist.tags.forEach(t => {
+        detail.data.playlist.tags.forEach(t => {
             span = $make('span');
             span.innerText = t;
             tag.append(span);
@@ -75,7 +95,7 @@ axios.all( [ getPlaylistDetail(), getPlaylistComment() ] )
 
         // 设置介绍
         let intro_content = $('.intro_content');
-        (deatil.data.playlist.description.split('\n')).forEach(s => {
+        (detail.data.playlist.description.split('\n')).forEach(s => {
             span = $make('span');
             if(s.length == 0)
                 s = '&nbsp;';
@@ -90,6 +110,8 @@ axios.all( [ getPlaylistDetail(), getPlaylistComment() ] )
         }
         else
         {
+            $('#intro_expand').checked = true;
+            $('#intro_expand').disabled = true;
             $('.intro_content > icon').style.display = 'none';
         }
     }
