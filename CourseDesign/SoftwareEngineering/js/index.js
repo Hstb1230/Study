38835deb4ef11502d451d547a6d55c9d6dbaa24d
Hstb1230 <i@hstb1230.com>
2020-06-17 08:07:13 +0800

    Rename starting() to reset(), Create resource.js

--- CourseDesign/SoftwareEngineering/js/index.js
+++ CourseDesign/SoftwareEngineering/js/index.js
@@ -1,61 +1,7 @@
 // 引用文件
 
 $include('./js/loadAnimation.js');
-
-// 背景图片
-let bg = new Image();
-bg.src = 'img/bg.jpg';
-
-// 全民飞机大战Logo
-let logo = new Image();
-logo.src = 'img/logo.png';
-
-// 我方战斗机
-let myPlane = new Image();
-myPlane.src = 'img/plane/my/plane.png';
-
-let myPlaneX = canvas.width / 2,
-    myPlaneY = 730;
-
-//战斗机子弹
-let bullet = new Image();
-bullet.src = 'img/bullet.png';
-let bullettime = 0,
-    bulletnum = 0,
-    bulletarr = [];
-//敌机
-let enemytime = 0,
-    enemyArr = [];
-let enemy1 = new Image();
-enemy1.src = `img/enemy1.png`;
-let enemy2 = new Image();
-enemy2.src = `img/enemy2.png`;
-let enemy3 = new Image();
-enemy3.src = `img/enemy3.png`;
-let enemy4 = new Image();
-enemy4.src = `img/enemy4.png`;
-let enemyall = [enemy1, enemy2, enemy3, enemy4];
-//战斗机爆炸
-let myplane1boom = new Image();
-let myboomnum = 1,
-    myboomtime = 0;
-//敌机爆炸
-let enemychangearr = [];
-//boss警告
-let warning1 = new Image();
-warning1.src = 'img/warning1.png';
-let warning2 = new Image();
-warning2.src = 'img/warning2.png';
-let warningTime = 0;
-let warningChange = 0;
-//boss出场背景
-let bossbg = new Image();
-bossbg.src = 'img/bg2.jpg';
-let boss = new Image();
-boss.src = 'img/planeboss.png';
-//boss改变飞机速度
-let bossattacktime = 0;
-let bossattacknum = 1;
+$include('./js/resource.js');
 
 let game = {
     // 游戏状态
@@ -126,9 +72,10 @@ let game = {
     },
 
     // 开始新一轮游戏，重置游戏状态
-    starting : function ()
+    reset : function ()
     {
         startBtn.classList.remove('none');
+
         game.life = 3;
         game.score = 0;
 
@@ -156,7 +103,6 @@ let game = {
         game.bossTimeBlur = true;
         game.bossAttack = false;
 
-        view.drawImage(logo, 110, 200);
     },
 
     myplane : function ( e )
@@ -500,7 +446,9 @@ setInterval(function ()
     game.bgChange();
     if( game.gamestart == 1 )
     {
-        game.starting();
+        game.reset();
+
+        view.drawImage(logo, 110, 200);
     }
     if( game.gameload == 1 )
     {
---
+++ CourseDesign/SoftwareEngineering/js/resource.js
@@ -0,0 +1,55 @@
+
+// 背景图片
+let bg = new Image();
+bg.src = 'img/bg.jpg';
+
+// 全民飞机大战Logo
+let logo = new Image();
+logo.src = 'img/logo.png';
+
+// 我方战斗机
+let myPlane = new Image();
+myPlane.src = 'img/plane/my/plane.png';
+
+let myPlaneX = canvas.width / 2,
+    myPlaneY = 730;
+
+//战斗机子弹
+let bullet = new Image();
+bullet.src = 'img/bullet.png';
+let bullettime = 0,
+    bulletnum = 0,
+    bulletarr = [];
+//敌机
+let enemytime = 0,
+    enemyArr = [];
+let enemy1 = new Image();
+enemy1.src = `img/enemy1.png`;
+let enemy2 = new Image();
+enemy2.src = `img/enemy2.png`;
+let enemy3 = new Image();
+enemy3.src = `img/enemy3.png`;
+let enemy4 = new Image();
+enemy4.src = `img/enemy4.png`;
+let enemyall = [enemy1, enemy2, enemy3, enemy4];
+//战斗机爆炸
+let myplane1boom = new Image();
+let myboomnum = 1,
+    myboomtime = 0;
+//敌机爆炸
+let enemychangearr = [];
+//boss警告
+let warning1 = new Image();
+warning1.src = 'img/warning1.png';
+let warning2 = new Image();
+warning2.src = 'img/warning2.png';
+let warningTime = 0;
+let warningChange = 0;
+//boss出场背景
+let bossbg = new Image();
+bossbg.src = 'img/bg2.jpg';
+let boss = new Image();
+boss.src = 'img/planeboss.png';
+//boss改变飞机速度
+let bossattacktime = 0;
+let bossattacknum = 1;
