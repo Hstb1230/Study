f9d3ee699613e3a4cde07b1969e774477f0923f7
Hstb1230 <i@hstb1230.com>
2020-06-25 01:15:06 +0800

    Set Float

--- CourseDesign/SoftwareEngineering/js/home.js
+++ CourseDesign/SoftwareEngineering/js/home.js
@@ -3,3 +3,19 @@ if(account.isLogin)
     $('.tab .default').style.display = 'none';
     $('.tab .home').style.display = 'black';
 }
+
+// 点击空白区域关闭悬浮窗（隐藏）
+document.addEventListener('click',
+    function ()
+    {
+        $('.float').style.display = 'none';
+    }
+);
+
+$('.float .container').addEventListener('click',
+    function (e)
+    {
+        e.stopPropagation();
+        return false;
+    }
+);
