f8c80bedbd1d71d83815dd28e9bbf2e03524d568
Hstb1230 <i@hstb1230.com>
2020-06-29 12:00:25 +0800

    Add portal, isLogin.

--- CourseDesign/SoftwareEngineering/api/admin/fnc.php
+++ CourseDesign/SoftwareEngineering/api/admin/fnc.php
@@ -55,3 +55,48 @@ function addCommodity($prop, $amount, $pay, & $reason)
         $reason = $stmt->error;
     return $success;
 }
+
+/**
+ * 登录验证
+ * @param string $username
+ * @param string $password
+ * @param string $reason
+ * @return bool
+ */
+function portal($username, $password, & $reason)
+{
+    global $conn;
+    $stmt = $conn->prepare('SELECT content FROM `setting` WHERE name = ? UNION SELECT content FROM `setting` WHERE name = ?');
+    $var1 = 'username';
+    $var2 = 'password';
+    $stmt->bind_param('ss', $var1, $var2);
+    $stmt->execute();
+    $stmt->store_result();
+    if($stmt->num_rows === 2)
+    {
+        $content = '';
+        $stmt->bind_result($content);
+        $stmt->fetch();
+        if($username !== $content)
+        {
+            $reason = '用户名错误';
+            goto end;
+        }
+        $stmt->fetch();
+        if($password !== $content)
+        {
+            $reason = '密码错误';
+            goto end;
+        }
+        $_SESSION['role'] = 1;
+        return true;
+    }
+    $reason = '记录不存在';
+    end:
+    return false;
+}
+
+function isLogin()
+{
+    return @$_SESSION['role'] === 1;
+}
