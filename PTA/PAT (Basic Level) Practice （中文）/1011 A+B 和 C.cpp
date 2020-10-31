ebfd285bf0c24a67ff8b528f2a3120eddf5068a0
Hstb1230 <i@hstb1230.com>
2020-10-31 20:54:15 +0800

    Update 1011 A+B 和 C，1018 锤子剪刀布
    Less and More.

--- PTA/PAT (Basic Level) Practice （中文）/1011 A+B 和 C.cpp
+++ PTA/PAT (Basic Level) Practice （中文）/1011 A+B 和 C.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 using namespace std;
 
+extern bool isMore0(int x)
+{
+	return !(x >> 31 & 1);
+}
+
 int main() {
-	int n;
-	cin >> n;
-	long long a, b, c;
-	for(int i = 1; i <= n; i++) {
-		cin >> a >> b >> c;
-		printf("Case #%d: ", i);
-		if(a + b > c) {
-			cout << "true";
-		} else {
-			cout << "false";
+    int T, a, b, c;
+    cin >> T;
+    for(int i = 1; i <= T; i++)
+    {
+        cin >> a >> b >> c;
+        if(a > b)
+            swap(a, b);
+		bool flag;
+		if(isMore0(a) != isMore0(b))  // ab异号
+			flag = (a + b > c);
+		else if(isMore0(a) != isMore0(c))  // ac异号
+		{
+			// a（正），b（正）一定大于c（负）
+			// a（负）一定小于c（正）
+			flag = (a > c);
 		}
-		cout << endl;
-	}
+		else  // abc同号
+		{
+			int d = a - c;
+			flag = (d > 0 || b + d > 0);
+		}
+		printf("Case #%d: %s\n", i, (flag ? "true" : "false"));
+    }
 }
--- PTA/PAT (Basic Level) Practice （中文）/1018 锤子剪刀布.cpp
+++ PTA/PAT (Basic Level) Practice （中文）/1018 锤子剪刀布.cpp
@@ -1,71 +1,49 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <algorithm>
+#include <cmath>
 using namespace std;
 
-int judge(const char & a, const char & b) {
-	// printf("%c %c\n", a, b);
-	if(a == b)
-		return 0;
-	if(
-		(a == 'C' && b == 'J') ||
-		(a == 'J' && b == 'B') ||
-		(a == 'B' && b == 'C')
-	)
-		return 1;
-	return 2;
+int cntA[3], cntB[3];  // 胜平负次数
+int winA[3], winB[3];  // 胜利手势统计
+const string str = "CJB";
+
+extern int c2i(char c)
+{
+	return str.find(c);
 }
 
-bool cmp(pair<char, int> a, pair<char, int> b) {
-//	printf("%c %d | %c %d\n", a.first, a.second, b.first, b.second);
-	if(a.second == b.second)
-		return a.first < b.first;
-	return a.second > b.second;
+extern char best(const int * win)
+{
+	int m = 0;
+	for(int i = 1; i < 3; i++)
+	{
+		if(win[i] > win[m] || (win[i] == win[m] && str[i] < str[m]))
+			m = i;
+	}
+	return str[m];
 }
 
-int main() {
+int main()
+{
 	int n;
 	cin >> n;
-	char ag, bg; // guess
-	int r; // fight result
-	map<char, int> ma, mb;
-	ma['C'] = ma['J'] = ma['B'] = 0;
-	mb['C'] = mb['J'] = mb['B'] = 0;
-	vector<int> va(3), vb(3);
-	while(n--) {
-		cin >> ag >> bg;
-		
-		r = judge(ag, bg);
-		va[r]++;
-		if(r == 1)
-			ma[ag]++;
-			
-		r = judge(bg, ag);
-		vb[r]++;
-		if(r == 1)
-			mb[bg]++;
-		
+	char a, b;
+	while(n--)
+	{
+		cin >> a >> b;
+		int x = c2i(a), y = c2i(b);  // 手势
+		int rA = 0, rB = 0;  // 默认平局
+		if(abs(x - y) == 1)
+		    rA = (x < y ? 1 : -1), rB = -rA;
+		else if(abs(x - y) == 2)
+		    rA = (x > y ? 1 : -1), rB = -rA;
+        rA++, rB++; // 负0，平1，胜2
+		cntA[rA]++, cntB[rB]++;
+		if(rA == 2)
+			winA[x]++;
+		else if(rB == 2)
+			winB[y]++;
 	}
-	// 输出甲乙的划拳统计结果
-	printf("%d %d %d\n", va[1], va[0], va[2]);
-	printf("%d %d %d\n", vb[1], vb[0], vb[2]);
-
-	vector< pair<char, int> > vm;
-	vm.assign(ma.begin(), ma.end());
-	// 不知道结构体比较怎么用, 或者说是不是其他标准才行
-//	struct {
-//		bool operator()(pair<char, int> a, pair<char, int> b) const {
-//			return a.second < b.second;
-//		}
-//	} cmp;
-	sort(vm.begin(), vm.end(), cmp);
-//	for(int i = 0; i < vm.size(); i++)
-//		printf("%c %d\n", vm[i].first, vm[i].second);
-	cout << vm[0].first;
-	cout << ' ';
-	
-	vm.assign(mb.begin(), mb.end());
-	sort(vm.begin(), vm.end(), cmp);
-	cout << vm[0].first;
+	printf("%d %d %d\n", cntA[2], cntA[1], cntA[0]);
+	printf("%d %d %d\n", cntB[2], cntB[1], cntB[0]);
+	printf("%c %c", best(winA), best(winB));
 }
