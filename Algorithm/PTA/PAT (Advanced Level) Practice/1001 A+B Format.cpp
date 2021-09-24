3ba34411c92a05c621d33080bde09e751c383938
Hstb1230 <Hstb1230@users.noreply.github.com>
2021-09-24 20:55:31 +0800

    Update 1001 A+B Format.cpp
    Simplify

--- Algorithm/PTA/PAT (Advanced Level) Practice/1001 A+B Format.cpp
+++ Algorithm/PTA/PAT (Advanced Level) Practice/1001 A+B Format.cpp
@@ -6,22 +6,16 @@ int main()
 	int a, b;
 	cin >> a >> b;
 	int c = a + b;
-	int num[10] = {1}, t = 0;
 	if(c < 0)
 	{
 		cout << '-';
 		c *= -1;
 	}
-	do
-	{
-		num[t++] = c % 1000;
-		c /= 1000;
-	} while(c != 0);
+	
+	int f[10] = { c % 1000 }, t = 1;
+	while(c /= 1000)
+		f[t++] = c % 1000;
+		
 	for(int i = 0; i < t; i++)
-	{
-		if(i > 0)
-			printf(",%03d", num[t - 1 - i]);
-		else
-			printf("%d", num[t - 1]);
-	}
+		printf(i ? ",%03d" : "%d", f[t - 1 - i]);
 }
